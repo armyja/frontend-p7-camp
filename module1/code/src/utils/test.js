088d2660bd75219a2dc2391c7e1af751e86683f6
Armyja <546323405@qq,com>
2021-07-21 01:32:04 +0800

    fix: 实现 evalInScope

--- module1/code/src/utils/test.js
+++ module1/code/src/utils/test.js
@@ -36,24 +36,27 @@ function _render(node, object) {
     let el;
     if ("text" === node.type) {
         let content = node.content;
-        el = document.createTextNode(process(content));
+        el = document.createTextNode(process(content, object));
         return el;
     }
     if ("element" === node.type) {
         el = document.createElement(node.tagName);
         for (let attr of node.attributes) {
-            if ("v-if" === attr.name && !evalInScope(attr.value, object)) {
-                return null;
+            if ("v-if" === attr.name) {
+                if (!evalInScope(attr.value, object)) {
+                    return null;
+                }
+                continue;
             }
             // todo 处理 {{}} 转换
-            if (attr.value.match(/^{{.+}}$/)) {
+            if (typeof attr.value === "string" && attr.value.match(/^{{.+}}$/)) {
                 el.setAttribute(attr.name, evalInScope(attr.value, object));
             } else {
                 el.setAttribute(attr.name, attr.value);
             }
         }
         for (let child of node.children) {
-            let childEl = _render(child, objectGetter);
+            let childEl = _render(child, object);
             if (childEl !== null) {
                 el.appendChild(childEl);
             }
@@ -63,7 +66,7 @@ function _render(node, object) {
 }
 
 function process(str, object) {
-    let pointer = str.matchAll("{{(.?)}}");
+    let pointer = str.matchAll("{{(.+?)}}");
     let current = 0;
     let result = "";
     while (true) {
@@ -71,19 +74,26 @@ function process(str, object) {
         if (tempResult.done) {
             break;
         }
+        
+        result += str.substring(current, tempResult.value.index);
         result += evalInScope(tempResult.value[1], object);
-        current = index + tempResult.value[0].length;
+        current = tempResult.value.index + tempResult.value[0].length;
     }
     result += str.substring(current, str.length);
     return result;
 }
 
 
+// function evalInScopeOld(js, contextAsScope) {
+//     //# Return the results of the in-line anonymous function we .call with the passed context
+//     return function() { 
+//         with(this) { 
+//             return eval(js);
+//          }; 
+//     }.call(contextAsScope);
+// }
+
 function evalInScope(js, contextAsScope) {
-    //# Return the results of the in-line anonymous function we .call with the passed context
-    return function() { 
-        with(this) { 
-            return eval(js);
-         }; 
-    }.call(contextAsScope);
+    let fn = new Function(`with(this){return eval("${js}");}`);
+    return fn.call(contextAsScope);
 }
